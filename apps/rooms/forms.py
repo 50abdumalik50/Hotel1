d2c7b7ffed58c439fb9adb189dbdd580298ba847
50abdumalik50 <abdumalikabdukarimov50@gmail.com>
2024-04-30 00:36:28 +0600

    first commit

---
+++ apps/rooms/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from django.forms import inlineformset_factory
+
+from apps.rooms.models import Room, Booking
+
+
+class RoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = [
+            # 'Room_choice',
+            'room_type',
+            'number',
+            'number_of_beds',
+            'price',
+            'startDate',
+            'endDate',
+        ]
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = [
+            'user',
+            'room',
+            'check_in_date',
+            'check_out_date',
+            'date_of_reservation',
+        ]
+
+
+# class ImagesForm(forms.ModelForm):
+#     class Meta:
+#         model = Images
+#         fields = ('image',)
+#         widgets = {'image': forms.ClearableFileInput(attrs={
+#                                             'class': 'form-control'
+#         })}
+#
+#
+# RoomImagesFormSet = inlineformset_factory(Room, Images, form=ImagesForm, extra=1, max_num=5)
